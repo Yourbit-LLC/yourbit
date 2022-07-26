f243f45a160a133484c5de93ccf0d86915ad8e79
achaney55 <aus.chaney@gmail.com>
2022-07-26 02:10:20 -0400

    another another yet another attempt at static files fix

--- YourbitCore/settings.py
+++ YourbitCore/settings.py
@@ -140,8 +140,12 @@ DATE_INPUT_FORMATS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "yourbit/static"),
+]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = "/django/yourbit/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
