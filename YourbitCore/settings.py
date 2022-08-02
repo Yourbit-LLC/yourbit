a8f371abeeee6781fad52092436162e2f393f7ee
achaney55 <aus.chaney@gmail.com>
2022-08-02 18:24:21 -0400

    animations-2

--- YourbitCore/settings.py
+++ YourbitCore/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['45.79.189.46', '127.0.0.1', 'yourbit.me', 'www.yourbit.me']
 
