c766f22174f24682e909b84932ab644c213c69c3
achaney55 <aus.chaney@gmail.com>
2022-08-02 18:13:26 -0400

    animations-2

--- YourbitCore/settings.py
+++ YourbitCore/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'YourbitCore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
