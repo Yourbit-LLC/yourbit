d2638ae776520420be13dc37c2a6469946176eca
achaney55 <aus.chaney@gmail.com>
2022-09-23 10:41:46 -0400

    v0.6.4.6 starting messages

--- social/static/scripts/global.js
+++ social/static/scripts/global.js
@@ -1,6 +1,3 @@
-const cookie = document.cookie;
-const csrf = document.getElementById('csrf-token').value;
-
 function getCSRF() {
     let cookie = document.cookie;
     let csrf = document.getElementById('csrf-token').value;
