6f07cd3e6231b2411e89d800a1752469fb904c53
achaney55 <aus.chaney@gmail.com>
2023-04-27 10:55:42 -0400

    Update urls.py
    Changed url to point to view instead of template file

--- YourbitAccounts/urls.py
+++ YourbitAccounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('verify_email/<str:token>/', verify_email, name='verify_email'),
     path('verify_error/', verify_email, name='verify_email_error'),
     path('email_confirmation/', EmailConfirmation.as_view(), name='email_confirmation'),
-    path('documents/terms/', TemplateView.as_view(template_name='YourbitAccounts/yb-terms.htm'), name='terms'),
+    path('documents/terms/', terms_view, name='terms'),
 ]
