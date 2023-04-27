b2f2166839498a02140caec0fdcb17213e16a491
achaney55 <aus.chaney@gmail.com>
2023-04-26 22:54:50 -0400

    made some fixes for bit context menus

--- main/static/scripts/yb-builders.js
+++ main/static/scripts/yb-builders.js
@@ -93,12 +93,15 @@ function BuildBit(bit, liked_bits, disliked_bits){
 
     
     let options_button = yb_createButton("options", `options-bit-${id}`, "bit-options");
-    
+    options_button.setAttribute("data-id", id);
     options_button.innerHTML = `<svg id="feedback-icon-source" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6 14q-.825 0-1.412-.588Q4 12.825 4 12t.588-1.413Q5.175 10 6 10t1.412.587Q8 11.175 8 12q0 .825-.588 1.412Q6.825 14 6 14Zm6 0q-.825 0-1.412-.588Q10 12.825 10 12t.588-1.413Q11.175 10 12 10t1.413.587Q14 11.175 14 12q0 .825-.587 1.412Q12.825 14 12 14Zm6 0q-.825 0-1.413-.588Q16 12.825 16 12t.587-1.413Q17.175 10 18 10q.825 0 1.413.587Q20 11.175 20 12q0 .825-.587 1.412Q18.825 14 18 14Z"/></svg>`
     new_bit.appendChild(options_button);
 
     options_button.addEventListener("click", function(event){
-        let this_element = event.currentTarget;
+        
+        let options_element = event.currentTarget;
+        let options_id = options_element.getAttribute("data-id");
+        let this_element = document.getElementById(`bit-${options_id}`);
         yb_buildContextMenu(this_element);
     });
 
