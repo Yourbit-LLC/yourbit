9383871decd1f7dca0d66e9d286da61f4acac8cd
achaney55 <aus.chaney@gmail.com>
2023-04-28 12:24:35 -0400

    changed margin bottom of terms container

--- YourbitAccounts/static/scripts/register.js
+++ YourbitAccounts/static/scripts/register.js
@@ -240,7 +240,7 @@ function termsStage(){
     terms_window.setAttribute('scrolling', 'yes');
     terms_window.setAttribute('width', '98%');
     terms_window.setAttribute('height', '70%');
-    terms_window.setAttribute('style', 'margin-left: auto; margin-right: auto; background-color: #ffffff; border-radius: 20px; overflow: scroll; filter: drop-shadow(0px 0px 5px #000000); -webkit-filter: drop-shadow(0px 0px 5px #000000);')
+    terms_window.setAttribute('style', 'margin-bottom: 10px; margin-left: auto; margin-right: auto; background-color: #ffffff; border-radius: 20px; overflow: scroll; filter: drop-shadow(0px 0px 5px #000000); -webkit-filter: drop-shadow(0px 0px 5px #000000);')
 
 
     terms_window.setAttribute('src', `${base_url}/accounts/documents/terms/`);
