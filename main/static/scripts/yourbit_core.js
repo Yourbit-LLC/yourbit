7d8727b9574ef16fd4c2c829d10716bd0f8b4536
achaney55 <aus.chaney@gmail.com>
2023-05-01 23:44:27 -0400

    changes to create bit menu to hopefully fix panel close

--- main/static/scripts/yourbit_core.js
+++ main/static/scripts/yourbit_core.js
@@ -70,8 +70,6 @@ function hideCreateBit() {
     // let current_url = document.getElementById('current_url').value;
     // history.pushState({}, "", `${current_url}/`);
     $('#cb-divider').hide();
-    $(create_bit).html("");
-
 }
 
 
