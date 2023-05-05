acd7485173de103227881e46e8c7a91849ee8705
achaney55 <aus.chaney@gmail.com>
2023-05-04 23:12:55 -0400

    fixed issues with color circle initialization

--- user_profile/static/scripts/yb-customization.js
+++ user_profile/static/scripts/yb-customization.js
@@ -22,12 +22,34 @@ MINI_PARAGRAPH = document.getElementById("mini-paragraph");
 MINI_PARAGRAPH_MOBILE = document.getElementById("preview-paragraph");
 BIT_PROFILE_IMAGE = document.getElementById("bit-profile-image-image");
 
+//Declare constants for all input elements
+const PROFILE_IMAGE_INPUT = document.getElementById("profile-image-input");
+const BACKGROUND_IMAGE_INPUT = document.getElementById("background-image-input");
+const PRIMARY_COLOR_INPUT = document.getElementById("primary-color-select");
+const SECONDARY_COLOR_INPUT = document.getElementById("accent-color-select");
+const ICON_COLOR_INPUT = document.getElementById("icon-color-select");
+const FEEDBACK_ICON_COLOR_INPUT = document.getElementById("feedback-icon-color-select");
+const FEEDBACK_ICON_BACKGROUND_INPUT = document.getElementById("feedback-icon-background-select");
+const BLUR_RADIUS_INPUT = document.getElementById("blur-radius-select");
+
+
+/*################################ 
+
+        History tracking
+
+#################################*/
+
+//Declare change history object: {"#input-id" : {"old": "old value", "new": "new value"}}
+var change_history = {};
+var history_length = Object.keys(change_history).length;
 
 //Declare constants for all input elements
 PAGES = ["#profile-images", "#ui-colors", "#bits", "#fonts"]
 
+//Custom data stores information downloaded from the server on initialization (Might be legacy)
 var custom_data = {}
 
+//Document ready function
 $(document).ready(function() {
     
     $('#back-to-home').fadeIn('slow');
@@ -66,14 +88,6 @@ $(document).ready(function() {
         MINI_BIT.style.backgroundColor = custom_data.primary_color;
         MINI_BIT_MOBILE.style.backgroundColor = custom_data.primary_color;
 
-        MINI_USER_NAME.style.color =custom.title_color;
-        MINI_USERNAME.style.color =custom.title_color;
-
-        MINI_TITLE.style.color = custom.title_color;
-        MINI_TITLE_MOBILE.style.color = custom.title_color;
-        MINI_PARAGRAPH.style.color = custom.text_color;
-        MINI_PARAGRAPH_MOBILE.style.color = custom.text_color;
-
         //Fill UI colors
         $("#primary-color-select").attr("value", custom_data.primary_color);
         $("#accent-color-select").attr("value", custom_data.secondary_color);
@@ -85,48 +99,81 @@ $(document).ready(function() {
         $("#feedback-background-color-select").attr("value", custom.feedback_icon_background);
         $("#feedback-icon-color-select").attr("value", custom.feedback_icon_color);
 
+    //Add event listeners
 
+        //Event listener for cancel button inside image selection containt
+        let selection_cancel_button = document.getElementById("cancel-button");
+        selection_cancel_button.addEventListener("click", function() {
+            yb_closeImageSelection();
+        });
+
+        //Event listener for navigating back one step on image options
+        let profile_image_back_button = document.getElementById("profile-cropper-back");
+        profile_image_back_button.addEventListener("click", function() {
+            console.log("clicked")
+            let state = this.getAttribute("data-state");
+            console.log(state)
+            if (state === "0"){
+                $("#profile-cropper").fadeOut();
+                $("#background-cropper").fadeOut();
+                $("#profile-images").fadeIn();
+            } else {
+                $("#profile-cropper-select-button").fadeIn();
+                $("#upload-button").fadeIn();
+                $("#profile-image-input").hide();
+            }
+        });
 
     });
 
-/* 
 
-        History tracking
-
-*/
-//Declare change history object: {"#input-id" : {"old": "old value", "new": "new value"}}
-var change_history = {};
-var history_length = Object.keys(change_history).length;
 
 //Event listener for color select
 $('.color-circle').click(function(){
     let this_name = $(this).attr("name");
+
+    //Primary color circle pressed
     if (this_name === "primary"){
+        change_history.append({"#primary-color-select": {"old": PRIMARY_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption('#primary-color-select', focusIn);
+
     }
+    //Secondary color circle pressed
     else if (this_name === "secondary"){
+        change_history.append({"#accent-color-select": {"old": SECONDARY_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption("#accent-color-select", focusIn);
     }
+    //Icon color circle pressed
     else if (this_name === "icon"){
+        change_history.append({"#icon-color-select": {"old": ICON_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption("#icon-color-select", focusIn);
     } 
+    //Title color circle pressed
     else if (this_name === "title"){
+        change_history.append({"#title-color-select": {"old": TITLE_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption("#title-color-select", focusIn);
     }
+    //Text color circle pressed
     else if (this_name === "text"){
+        change_history.append({"#text-color-select": {"old": TEXT_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption("#text-color-select", focusIn);
     }
+    //Feedback icon color circle pressed
     else if (this_name === "fb_icon"){
+        change_history.append({"#feedback-icon-color-select": {"old": FEEDBACK_ICON_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption("#feedback-icon-color-select", focus);
 
-    } else if (this_name === "fb_background"){
+    }
+    //Feedback background color circle pressed
+    else if (this_name === "fb_background"){
+        change_history.append({"#feedback-background-color-select": {"old": FEEDBACK_BACKGROUND_COLOR_INPUT.value, "new": "awaiting user input..."}});
         showColorOption('feedback-background-color-select');
 
     }
 
 });
     
-//Event listener for image select button
+//Event listener for profile image upload button
 let profile_image_edit_button = document.getElementById("edit-profile-img");
 profile_image_edit_button.addEventListener("click", function(){
     $("#profile-images").fadeOut();
@@ -135,6 +182,7 @@ profile_image_edit_button.addEventListener("click", function(){
 
 });
 
+//Event listener for background image upload button
 let bkd_image_edit_button = document.getElementById("edit-background-img");
 bkd_image_edit_button.addEventListener("click", function(){
     $("#profile-images").fadeOut();
@@ -147,6 +195,8 @@ bkd_image_edit_button.addEventListener("click", function(){
     
 });
 
+
+//Function for showing image selection grid container for selecting from previous uploads
 function showImageSelection() {
     let selector_container = document.getElementById("image-selector");
     selector_container.style.display = "block";
@@ -174,20 +224,9 @@ function yb_createImageGrid(data) {
         container.appendChild(img);
     })
 }
-/* let uploadButton = document.getElementById("image-upload-button");
-uploadButton.addEventListener("click", function(){
-    if ($(this).attr("data-state") === "upload"){
-        $("#profile-img-upload-field").fadeIn();
-        $(this).html("Cancel");
-        $(this).attr("data-state", "cancel")
-    } else {
-        $("#profile-img-upload-field").fadeOut();
-        $(this).html("Upload");
-        $(this).attr("data-state", "upload")
-    }
-});*/
 
 
+//Legacy functions to be migrated to scrapyard
 $(".filter-button").click(function() {
     let this_name = $(this).attr("name");
     $(".dropdown-large").fadeOut();
@@ -207,23 +246,30 @@ $(".filter-button-wide-active").click(function() {
 $('#back-to-home').click(function() {
     window.location.href = `${base_url}/bitstream/home/`
 });
+//End legacy functions
 
 $('#bit-colors-preview').click(function() {
     showColorOption('#advanced-color-bit-background', focusIn)
     
 });
 
+//Show color option ("#field-id", callback_function)
 function showColorOption(field, callback) {
     $(field).show();
     callback(field);
 }
+
+//Focus on field for color option to reveal selector ("#field-id")
 function focusIn(field){
     $(field).focus();
 }
 
+//Hide color option ("#field-id")
 function hideColorOption(field) {
     $(field).hide();
 }
+
+//Blur out of field for color option to hide selector ("#field-id")
 function focusOut(field){
     $(field).blur();
 }
@@ -232,49 +278,12 @@ var profile_image_loaded;
 
 var preview_stage = 0;
 
-
+//Function for hiding an element by node
 function hideElement(sub_setting) {
     sub_setting.style.display = "none";
 }
 
-
-/* let profile_bkd_field = document.getElementById('bkd-image-field');
-profile_bkd_field.addEventListener("change", function() {
-    let image_preview = document.getElementById('preview-background-img');
-    const image_loaded = this.files[0];
-    if (image_loaded) {
-        const reader = new FileReader();
-
-        reader.addEventListener('load', function() {
-            image_preview.setAttribute('src', this.result);
-            updateCustom('image_upload', 'background_image', image_loaded)
-        });
-
-        reader.readAsDataURL(image_loaded);
-
-    }
-}); */
-
-/*let profile_img_field = document.getElementById('profile-img-upload-field');
-profile_img_field.addEventListener("change", function() {
-    let user_image_preview = document.getElementById('profile-image-preview-image');
-    const image_loaded = this.files[0];
-    if (image_loaded) {
-        const reader = new FileReader();
-
-        reader.addEventListener('load', function() {
-
-            updateCustom('image_upload', 'profile_image', image_loaded)
-            $("#profile-img-upload-field").fadeOut();
-            $("#image-upload-button").html("Upload");
-            $("#image-upload-button").attr("data-state", "upload");
-        });
-
-        reader.readAsDataURL(image_loaded);
-
-    }
-}); */
-
+//Event listener for global toggle colors switch
 $('#toggle-colors').change(function() {
     let color_option = document.getElementById('color-customizations');
     if ( color_option.style.display === 'none') {
@@ -285,6 +294,7 @@ $('#toggle-colors').change(function() {
     }
 }); 
 
+//AI contrast color request, may be replaced by built in function
 function yb_ContrastColor(value){
     let csrfToken = getCSRF();
     let request = new FormData();
@@ -318,54 +328,105 @@ function yb_ContrastColor(value){
 }
 
 
+/*############################################################################
+
+    
+    CHANGE LISTENERS
+    Functions for listening to changes in the color picker fields and updating
+
+
+##############################################################################*/
+
+//Change event listener for primary color field
 const primary_color_field = document.getElementById('primary-color-select')
 primary_color_field.addEventListener("change", function() {
+
+    //Set preview element = to bit background
     let bit_background = document.getElementById('bit-colors-preview');
     
+    //Update color circle to match color setting
     $("#color-circle-primary").css("transition", "0.5s");
     $("#color-circle-primary").css("background-color", this.value);
+    
+    //Hide the color selection field
     hideColorOption('#primary-color-select');
 
+    //Update history
+    change_history["primary-color-select"]["new"] = this.value; 
 
-
+    //Update bit background color
     bit_background.style.backgroundColor = this.value;
+    
+    //Update customizations ajax
     updateCustom('color_change', 'primary', this.value)
 });
 
+//Change event listener for secondary color field
 const accent_color_field = document.getElementById('accent-color-select')
 accent_color_field.addEventListener("change", function() {
+
+    //Set preview elements
     let border1 = document.getElementById('accent-color-preview');
     let border2 = document.getElementById('bit-profile-image');
+
+    //Update border colors of preview elements
     border1.style.borderColor = this.value;
     border2.style.borderColor = this.value;
     $('#profile-pic').css({'border-color': this.value});
+    
+    //Update customizations ajax
     updateCustom('color_change', 'secondary', this.value);
 
+    //Update history
+    change_history["#accent-color-select"]["new"] = this.value;
+
+    //Update color circle to match color setting
     $("#color-circle-secondary").css("transition", "0.5s");
     $("#color-circle-secondary").css("background-color", this.value);
+    
+    //Hide the color selection field
     hideColorOption('#accent-color-select');
     
 });
 
+//Change event listener for text color field
 const feedback_icon_field = document.getElementById('feedback-icon-color-select');
 feedback_icon_field.addEventListener('change', function() {
+    
+    //Update color circle to match color setting
     $("#color-circle-fb-icon").css("transition", "0.5s");
     $("#color-circle-fb-icon").css("background-color", this.value);
+
+    //Update preview elements
     let feedback_preview1 = document.getElementById('button1').style.backgroundColor = this.value;
     let feedback_preview2 = document.getElementById('button2').style.backgroundColor = this.value;
     let feedback_preview3 = document.getElementById('button3').style.backgroundColor = this.value;
     let feedback_preview4 = document.getElementById('button4').style.backgroundColor = this.value;
     let feedback_preview5 = document.getElementById('button5').style.backgroundColor = this.value;
+    
+    //Update customizations ajax
     updateCustom('color_change', 'feedback_icon', this.value);
+
+    //Update history
+    change_history["#feedback-icon-color-select"]["new"] = this.value;
+
+    //Hide the color selection field
     hideColorOption('#feedback-icon-color-select');
+
+    //Update history
+    change_history["#feedback-icon-color-select"]["new"] = this.value;
     
 });
 
 
 const icon_field = document.getElementById('icon-color-select');
 icon_field.addEventListener('change', function() {
+
+    //Update color circle to match color setting
     $("#color-circle-icon").css("transition", "0.5s");
     $("#color-circle-icon").css("background-color", this.value);
+
+    //Update preview elements
     let space_icon1 = document.getElementById('icon-image-1').style.fill = this.value;
     let space_icon2 = document.getElementById('icon-image-2').style.fill = this.value;
     let space_icon3 = document.getElementById('icon-image-3').style.fill = this.value;
@@ -377,38 +438,80 @@ icon_field.addEventListener('change', function() {
     $('.space-button-icon').css({'fill':this.value});
     $('.mobile-floating-actions-path').css({'fill': this.value});
     $('.mobile-floating-actions').css({'border-color': this.value});
+
+    //Update history
+    change_history["#icon-color-select"]["new"] = this.value;
+
+    //Update customizations ajax
     updateCustom('color_change', 'icon', this.value);
+    
+    //Hide the color selection field
     hideColorOption('#icon-color-select');
     
 });
 
+//Change event listener for text color field
 const text_color_field = document.getElementById('pfont-color-select');
 text_color_field.addEventListener('change', function() {
+    //Update color circle to match color setting
     $("#color-circle-text").css("transition", "0.5s");
     $("#color-circle-text").css("background-color", this.value);
     let text_color_preview = document.getElementById('preview-paragraph');
     text_color_preview.style.color = this.value;
+
+    //Update history
+    change_history["#pfont-color-select"]["new"] = this.value;
+
+    //Update customizations ajax
     updateCustom('color_change', 'paragraph_font', this.value);
+
+    //Hide the color selection field
     hideColorOption('#text-color-select');
 });
 
+//Change event listener for text color field
 const title_color_field = document.getElementById('tfont-color-select');
 title_color_field.addEventListener('change', function() {
+    //Update color circle to match color setting
     $("#color-circle-title").css("transition", "0.5s");
     $("#color-circle-title").css("background-color", this.value);
+
+    //Update preview elements
     let title_color_preview = document.getElementById('preview-title');
     title_color_preview.style.color = this.value;
+
+    //Update history
+    change_history["#tfont-color-select"]["new"] = this.value;
+
+    //Update customizations ajax
     updateCustom('color_change', 'title_font', this.value);
+
+    //Hide the color selection field
     hideColorOption('#tfont-color-select');
 });
 
+//Change event listener for text color field
 const paragraph_align_field = document.getElementById('yb-align-text');
 paragraph_align_field.addEventListener('change', function() {
+
+    //Update preview elements
     $('#preview-paragraph').css({'text-align': this.value});
+
+    //Update history
+    change_history["#yb-align-text"]["new"] = this.value;
+
+    //Update customizations ajax
     updateCustom('text_edit', 'para-align', this.value);
     
 });
 
+//Close image selection container
+function yb_closeImageSelection(){
+    let selector_container = document.getElementById("image-selector");
+    $("#image-selector").animate({"height": "0vh", "width":"0vw"}, "fast").fadeOut();
+}
+
+//Legacy Code for the old color picker to be removed after any potential conflicts ruled out
 accent_color_field.addEventListener('click', function() {
     if (preview_stage === 1) {
         $('#bit-colors-preview').fadeOut();
@@ -422,6 +525,7 @@ accent_color_field.addEventListener('click', function() {
     preview_stage = 0
 });
 
+
 icon_field.addEventListener('click', function() {
     if (preview_stage === 0) {
         $('#accent-color-preview').fadeOut();
@@ -434,6 +538,7 @@ icon_field.addEventListener('click', function() {
     preview_stage = 2
 });
 
+
 title_color_field.addEventListener('click', function() {
     if (preview_stage === 0) {
         $('#accent-color-preview').fadeOut();
@@ -445,6 +550,7 @@ title_color_field.addEventListener('click', function() {
     }
     preview_stage = 1
 });
+
 
 text_color_field.addEventListener('click', function() {
     if (preview_stage === 0) {
@@ -469,6 +575,8 @@ feedback_icon_field.addEventListener('click', function() {
     }
     preview_stage = 1
 });
+
+
 primary_color_field.addEventListener('click', function() {
     if (preview_stage === 0) {
         $('#accent-color-preview').fadeOut();
@@ -493,3 +601,5 @@ paragraph_align_field.addEventListener('click', function() {
     }
     preview_stage = 1
 });
+//End of legacy code
+
