365ab8a6a2a0836abcbbc7c510191aa561886bfa
achaney55 <aus.chaney@gmail.com>
2023-05-05 14:04:58 -0400

    i keep being an idiot, fixed the backend and corrected the field name reference in views for updating new user on customization

--- user_profile/views.py
+++ user_profile/views.py
@@ -669,4 +669,5 @@ class CustomNewUser(View):
         profile = Profile.objects.get(user=request.user)
         custom_object = Custom.objects.get(profile = profile)
         custom_object.is_new_user = False
+        print("Object value: " + custom_object.is_new_user)
         return JsonResponse({'success':'success'})
