838fe68aeca7a625e11b5d84765e70babfec163d
achaney55 <aus.chaney@gmail.com>
2023-05-09 19:46:48 -0400

    made a fix to the get feed function on history page

---
+++ user_profile/static/scripts/yb-history.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    let dataset = $("#yb-browse-nav").attr("data-dataset"); 
+    
+    let filter = yb_getSessionValues("filter");
+    let sort = yb_getSessionValues("sort");
+    let type = yb_getSessionValues("space");
+
+
+    let data = {
+        "type": type,
+        "dataset":dataset,
+        "filter": filter,
+        "sort": sort,
+
+    }
+
+    yb_getFeed(data, hideSplash, true);
+
+});
