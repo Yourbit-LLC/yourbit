ac3e5b909794d383794a3bf0d63c0c1fe826e96f
achaney55 <aus.chaney@gmail.com>
2023-05-16 19:06:37 -0400

    added tracking of original values for undo in privacy settings

--- settings/static/scripts/yb-privacy.js
+++ settings/static/scripts/yb-privacy.js
@@ -128,18 +128,21 @@ function PrefillForm() {
 
             const REAL_NAME_SELECT = document.getElementById("yb-field-real-name");
             REAL_NAME_SELECT.value = data.real_name_visibility;
+            original_values["real-name"] = data.real_name_visibility;
             REAL_NAME_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("real-name");
             });
 
             const BIRTHDAY_SELECT = document.getElementById("yb-field-birthday");
             BIRTHDAY_SELECT.value = data.birthday_visibility;
+            original_values["birthday"] = data.birthday_visibility;
             BIRTHDAY_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("birthday");
             });
 
             const PHONE_NUM_SELECT = document.getElementById("yb-field-phone-number");
             PHONE_NUM_SELECT.value = data.phone_number_visibility;
+            original_values["phone-number"] = data.phone_number_visibility;
             PHONE_NUM_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("phone-number");
             });
@@ -147,6 +150,7 @@ function PrefillForm() {
 
             const EMAIL_SELECT = document.getElementById("yb-field-email");
             EMAIL_SELECT.value = data.email_visibility;
+            original_values["email"] = data.email_visibility;
             EMAIL_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("email");
             });
@@ -154,6 +158,7 @@ function PrefillForm() {
 
             const FRIEND_COUNT_SELECT = document.getElementById("yb-field-friend-count");
             FRIEND_COUNT_SELECT.value = data.friend_count_visibility;
+            original_values["friend-count"] = data.friend_count_visibility;
             FRIEND_COUNT_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("friend-count");
             });
@@ -161,6 +166,7 @@ function PrefillForm() {
 
             const FOLLOW_COUNT_SELECT = document.getElementById("yb-field-follow-count");
             FOLLOW_COUNT_SELECT.value = data.follower_count_visibility;
+            original_values["follow-count"] = data.follower_count_visibility;
             FOLLOW_COUNT_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("follow-count");
             });
