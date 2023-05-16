afac107691584880d46ae80f002be1ddef949936
achaney55 <aus.chaney@gmail.com>
2023-05-16 18:00:24 -0400

    fixes to incorrect ids in script

--- settings/static/scripts/yb-privacy.js
+++ settings/static/scripts/yb-privacy.js
@@ -126,40 +126,40 @@ function PrefillForm() {
             $("#yb-check-reach").prop("checked", data.friends_of_friends);
             $("#yb-check-real-search").prop("checked", data.search_by_name);
 
-            const REAL_NAME_SELECT = document.getElementById("yb-select-real-name");
+            const REAL_NAME_SELECT = document.getElementById("yb-field-real-name");
             REAL_NAME_SELECT.value = data.real_name_visibility;
             REAL_NAME_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("real-name");
             });
 
-            const BIRTHDAY_SELECT = document.getElementById("yb-select-birthday");
+            const BIRTHDAY_SELECT = document.getElementById("yb-field-birthday");
             BIRTHDAY_SELECT.value = data.birthday_visibility;
             BIRTHDAY_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("birthday");
             });
 
-            const PHONE_NUM_SELECT = document.getElementById("yb-phone-number-select");
+            const PHONE_NUM_SELECT = document.getElementById("yb-field-phone-number");
             PHONE_NUM_SELECT.value = data.phone_number_visibility;
             PHONE_NUM_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("phone-number");
             });
 
 
-            const EMAIL_SELECT = document.getElementById("yb-select-email");
+            const EMAIL_SELECT = document.getElementById("yb-field-email");
             EMAIL_SELECT.value = data.email_visibility;
             EMAIL_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("email");
             });
 
 
-            const FRIEND_COUNT_SELECT = document.getElementById("yb-select-friend-count");
+            const FRIEND_COUNT_SELECT = document.getElementById("yb-field-friend-count");
             FRIEND_COUNT_SELECT.value = data.friend_count_visibility;
             FRIEND_COUNT_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("friend-count");
             });
 
 
-            const FOLLOW_COUNT_SELECT = document.getElementById("yb-select-follow-count");
+            const FOLLOW_COUNT_SELECT = document.getElementById("yb-field-follow-count");
             FOLLOW_COUNT_SELECT.value = data.follower_count_visibility;
             FOLLOW_COUNT_SELECT.addEventListener("change", function() {
                 yb_handleInputChange("follow-count");
@@ -199,7 +199,7 @@ function PrefillForm() {
                 yb_saveCheck("real-search", this.checked);
             }
             );
-            
+
 
 
 
