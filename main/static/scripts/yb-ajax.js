4a9054841ee4f36cd3f57c89260f6cd577458548
achaney55 <aus.chaney@gmail.com>
2023-05-28 13:47:14 -0400

    added some overflow management

--- main/static/scripts/yb-ajax.js
+++ main/static/scripts/yb-ajax.js
@@ -1127,7 +1127,8 @@ function yb_listClusters(bit_id=null) {
                         let id = clusters[i].id;
                         let this_item = yb_createElement("p", `list-option-${id}`, "bit-context-option");
                         this_item.innerHTML = text;
-
+                        this_item.setAttribute("data-id", id);
+                        this_item.setAttribute("style", "margin-top: 3px; margin-bottom: 3px;");
                         //Append this item to this container
                         this_container.appendChild(this_item);
 
