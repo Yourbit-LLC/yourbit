099329e68a0700fa7cfb105d7db511388e128d23
achaney55 <aus.chaney@gmail.com>
2023-06-18 00:22:22 -0400

    ...

--- main/static/scripts/yourbit_core.js
+++ main/static/scripts/yourbit_core.js
@@ -968,33 +968,44 @@ function yb_openImage(source, index, this_id){
 
                 viewer.appendChild(interaction_container);
                 $('.photo-viewer').fadeIn(200);
+                $('.photo-viewer').animate({"top": "0px"}, 200);
+            
                 // Retrieve the #photo-viewer element
                 var photoViewer = document.getElementById("photo-viewer");
 
                 // Attach an event listener for touchstart event
                 photoViewer.addEventListener("touchstart", function(event) {
-                var initialY = event.touches[0].clientY;
+                    var initialY = event.touches[0].clientY;
 
-                // Add an event listener for touchend event
-                photoViewer.addEventListener("touchend", function(event) {
-                    var finalY = event.changedTouches[0].clientY;
-                    var deltaY = finalY - initialY;
+                    // Add an event listener for touchend event
+                    photoViewer.addEventListener("touchend", function(event) {
+                        yb_swipeDown("photo-viewer", event, initialY);
+                    });
 
-                    // Check if the user has swiped down
-                    if (deltaY > 0) {
-                    // Perform actions to exit fullscreen
-                    // Add your code here to handle fullscreen exit
-                    console.log("Swiped down to exit fullscreen!");
-                    alert("Swiped down to exit fullscreen!");
-                    }
+                    
                 });
-});
-                
+                    
             }
         } 
     );
 
     
+}
+
+function yb_swipeDown(option, event, initialY) {
+    if (option == "photo-viewer") {
+        var finalY = event.changedTouches[0].clientY;
+        var deltaY = finalY - initialY;
+
+        // Check if the user has swiped down
+        if (deltaY > 0) {
+            // Perform actions to exit fullscreen
+            // Add your code here to handle fullscreen exit
+            console.log("Swiped down to exit fullscreen!");
+            $('.photo-viewer').animate({"top": "100vh"}, 200);
+            $('.photo-viewer').fadeOut(200);
+        }
+    }
 }
 
 // const menuContainer = document.querySelector('.menu-container');
