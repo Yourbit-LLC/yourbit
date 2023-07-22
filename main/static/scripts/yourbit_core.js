b839c136e4561a07bc1d47115d232967e1bcf8c7
achaney55 <aus.chaney@gmail.com>
2023-07-22 01:25:17 -0400

    trying an experiment

--- main/static/scripts/yourbit_core.js
+++ main/static/scripts/yourbit_core.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
             var deltaY = finalY - initialY;
     
             // Check if the user has swiped down
-            if (deltaY > 150) {
+            if (deltaY > 250) {
                 console.log(deltaY);
                 // Perform actions to exit fullscreen
                 // Add your code here to handle fullscreen exit
