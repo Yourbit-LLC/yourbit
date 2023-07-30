edcbbefe221210a082970846b0b79e461039474e
achaney55 <aus.chaney@gmail.com>
2023-07-30 04:05:50 -0400

    added icon to schedule button

--- main/static/scripts/yb-create-menu.js
+++ main/static/scripts/yb-create-menu.js
@@ -381,11 +381,13 @@ function yb_createBitOptionsForm(option_field){
     //Append scope options to scope option element
     scope_options.appendChild(private_button);
     scope_options.appendChild(public_button);
+    scope_options.style.gridColumn = "1";
 
     bit_options.appendChild(scope_options);
 
     let schedule_button = yb_createButton("toggle_schedule", "bit-schedule", "bit-options-button", `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-300q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/></svg><p>Schedule</p>`);
     schedule_button.setAttribute("type","button");
+    schedule_button.style.gridColumn = "2";
     schedule_button.addEventListener("click", function() {
         console.log("clicked schedule");
     });
@@ -394,6 +396,7 @@ function yb_createBitOptionsForm(option_field){
     //Attach script to form
     let enhance_button = yb_createButton("button", "yb-enhance-button", "bit-options-button", '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="m800 376-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82Zm-460 0-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82Zm460 460-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82ZM204 964 92 852q-12-12-12-29t12-29l446-446q12-12 29-12t29 12l112 112q12 12 12 29t-12 29L262 964q-12 12-29 12t-29-12Zm30-84 286-288-56-56-288 286 58 58Z"/></svg><p>Enhance</p>');
     enhance_button.setAttribute("type", "button");
+    enhance_button.style.gridColumn = "3";
     
     
     //Append enhance button to bit options
