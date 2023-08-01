f00a56eae28a48dad2f280ec1c2e501fd69b19ca
achaney55 <aus.chaney@gmail.com>
2023-08-01 03:04:52 -0400

    fixed issue with not being able to close

--- main/static/scripts/yb-create-menu.js
+++ main/static/scripts/yb-create-menu.js
@@ -345,9 +345,43 @@ function yb_toggleScope(this_element){
 }
 
 
+function yb_closeBitOption(this_element, action, menu_element) {
+    
+    
+    this_element.setAttribute("data-state", "0");
+    this_element.removeEventListener("click", yb_closeBitOption);
+    menu_element.remove();
+    
+    if (action === "schedule"){
+        
+        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-300q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/>`;
+    } else if (action === "evaporate"){
+        
+        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-720v520-520Zm170 600H280q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v172q-17-5-39.5-8.5T680-560v-160H280v520h132q6 21 16 41.5t22 38.5Zm-90-160h40q0-63 20-103.5l20-40.5v-216h-80v360Zm160-230q17-11 38.5-22t41.5-16v-92h-80v130ZM680-80q-83 0-141.5-58.5T480-280q0-83 58.5-141.5T680-480q83 0 141.5 58.5T880-280q0 83-58.5 141.5T680-80Zm66-106 28-28-74-74v-112h-40v128l86 86Z"/></svg>`
+    } else if (action === "monetize"){
+     
+        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M444-200h70v-50q50-9 86-39t36-89q0-42-24-77t-96-61q-60-20-83-35t-23-41q0-26 18.5-41t53.5-15q32 0 50 15.5t26 38.5l64-26q-11-35-40.5-61T516-710v-50h-70v50q-50 11-78 44t-28 74q0 47 27.5 76t86.5 50q63 23 87.5 41t24.5 47q0 33-23.5 48.5T486-314q-33 0-58.5-20.5T390-396l-66 26q14 48 43.5 77.5T444-252v52Zm36 120q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
+    } else if (action === "enhance"){
+        
+        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="m800 376-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82Zm-460 0-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82Zm460 460-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82ZM204 964 92 852q-12-12-12-29t12-29l446-446q12-12 29-12t29 12l112 112q12 12 12 29t-12 29L262 964q-12 12-29 12t-29-12Zm30-84 286-288-56-56-288 286 58 58Z"/></svg>`
+    }
+
+    //Event Listener for opening menu
+    this_element.addEventListener("click", function() {
+        yb_handleBitOption(this_element)
+    });
+}
+
+
 function yb_scheduleMenu(this_element) {
     let menu_element = yb_createElement("div", "schedule-menu", "yb-options-up");
     menu_element.setAttribute("style", "width: 90%; margin: 0 auto;");
+
+    //Event Listener for closing menu
+    this_element.removeEventListener("click", yb_handleBitOption);
+    this_element.addEventListener("click", function() {
+        yb_closeBitOption(this_element, action , menu_element)
+    });
 
     let parent_element = this_element.parentElement;
 
@@ -387,48 +421,11 @@ function yb_scheduleMenu(this_element) {
     parent_element.appendChild(menu_element);
 }
 
-function yb_closeBitOption(this_element, action) {
-    
-    
-    this_element.setAttribute("data-state", "0");
-    this_element.removeEventListener("click", yb_closeBitOption);
-    
-    if (action === "schedule"){
-        let schedule_menu = document.getElementById("schedule-menu");
-        schedule_menu.remove();
-        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-300q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/>`;
-    } else if (action === "evaporate"){
-        let evaporate_menu = document.getElementById("evaporate-menu");
-        evaporate_menu.remove();
-
-        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-720v520-520Zm170 600H280q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v172q-17-5-39.5-8.5T680-560v-160H280v520h132q6 21 16 41.5t22 38.5Zm-90-160h40q0-63 20-103.5l20-40.5v-216h-80v360Zm160-230q17-11 38.5-22t41.5-16v-92h-80v130ZM680-80q-83 0-141.5-58.5T480-280q0-83 58.5-141.5T680-480q83 0 141.5 58.5T880-280q0 83-58.5 141.5T680-80Zm66-106 28-28-74-74v-112h-40v128l86 86Z"/></svg>`
-    } else if (action === "monetize"){
-        let monetize_menu = document.getElementById("monetize-menu");
-        monetize_menu.remove();
-        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M444-200h70v-50q50-9 86-39t36-89q0-42-24-77t-96-61q-60-20-83-35t-23-41q0-26 18.5-41t53.5-15q32 0 50 15.5t26 38.5l64-26q-11-35-40.5-61T516-710v-50h-70v50q-50 11-78 44t-28 74q0 47 27.5 76t86.5 50q63 23 87.5 41t24.5 47q0 33-23.5 48.5T486-314q-33 0-58.5-20.5T390-396l-66 26q14 48 43.5 77.5T444-252v52Zm36 120q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
-    } else if (action === "enhance"){
-        let enhance_menu = document.getElementById("enhance-menu");
-        enhance_menu.remove();
-        this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="m800 376-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82Zm-460 0-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82Zm460 460-38-82-82-38 82-38 38-82 38 82 82 38-82 38-38 82ZM204 964 92 852q-12-12-12-29t12-29l446-446q12-12 29-12t29 12l112 112q12 12 12 29t-12 29L262 964q-12 12-29 12t-29-12Zm30-84 286-288-56-56-288 286 58 58Z"/></svg>`
-    }
-
-    //Event Listener for opening menu
-    this_element.addEventListener("click", function() {
-        yb_handleBitOption(this_element)
-    });
-}
-
 function yb_handleBitOption(this_element){
     
     //Get button name to identify action
     let action = this_element.getAttribute("name");
     this_element.setAttribute("data-state", "1");
-
-    //Event Listener for closing menu
-    this_element.removeEventListener("click", yb_handleBitOption);
-    this_element.addEventListener("click", function() {
-        yb_closeBitOption(this_element, action)
-    });
 
     //Change icon to close
     this_element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
