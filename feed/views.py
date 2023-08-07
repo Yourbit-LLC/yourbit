a603f74b58709bb350a3d0eab12376d76e6ac7c0
achaney55 <aus.chaney@gmail.com>
2023-08-06 22:56:50 -0400

    more work on trying to get streaming up

--- feed/views.py
+++ feed/views.py
@@ -720,8 +720,8 @@ def video_stream(request, video_id):
     video_key = this_bit.video_key
 
     #Read AWS credentials from environment variables
-    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    aws_access_key_id = os.environ.get('BUCKET_ACCESS_KEY')
+    aws_secret_access_key = os.environ.get('BUCKET_SECRET_KEY')
 
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
