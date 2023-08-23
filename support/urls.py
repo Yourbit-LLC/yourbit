189bb614c5fc3885108443f76ee83077a752d76d
achaney55 <aus.chaney@gmail.com>
2023-08-23 03:57:32 -0400

    still working on subdomains

--- support/urls.py
+++ support/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path, re_path
 from .views import *
 from user_profile.api.viewsets import follow, requestFriend, updateTimezone, likeBit, dislikeBit, commentBit, shareBit
-from .utils import subdomain_reverse
-
-support_home_url = subdomain_reverse('', subdomain='support')
-support_faq_url = subdomain_reverse('faq', subdomain='support')
-support_contact_url = subdomain_reverse('contact', subdomain='support')
 
 urlpatterns = [
     
