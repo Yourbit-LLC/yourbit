660c2471a746a114e83386c104ced3c165928fff
achaney55 <aus.chaney@gmail.com>
2023-08-30 04:32:44 -0400

    finished up revert colors

--- main/static/scripts/yb-profiles.js
+++ main/static/scripts/yb-profiles.js
@@ -33,7 +33,7 @@ function yb_setProfileUI(custom) {
 
     let ui_icons = document.getElementsByClassName("yb-ui-icon");
     for (let i = 0; i < ui_icons.length; i++) {
-        ui_icons[i].setAttribute("style", `color:${custom.icon_color} !important;`);
+        ui_icons[i].setAttribute("style", `fill:${custom.icon_color} !important;`);
     }
 
     let accent_buttons = document.getElementsByClassName("yb-accent-button");
