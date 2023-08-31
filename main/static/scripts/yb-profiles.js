fa941a4360ba904228e5817a4c24dd234290a30a
achaney55 <aus.chaney@gmail.com>
2023-08-31 00:14:32 -0400

    worked on some stuff on profile

--- main/static/scripts/yb-profiles.js
+++ main/static/scripts/yb-profiles.js
@@ -51,6 +51,53 @@ function yb_setProfileUI(custom) {
 
 }
 
+function yb_enterProfile() {
+    console.log("clicked")
+    let profile_splash = document.getElementById("profile-page-splash");
+    let image = document.getElementById("profile-image-splash");
+    let profile_id = yb_getSessionValues("profile-username");
+    
+
+    $("#profile-splash-label").animate({"height": "80px", "top":"100px", "width": "100%"});
+    $("#splash-bio-container").css({"display": "none"});
+    $("#profile-splash-label").css({"display": "grid", "text-align":"left", "grid-template-columns":"60px auto 120px", "padding-top":"230px"});
+    $("#profile-name-header").css({"font-size":"15px", "margin-left":"10px", "margin-top":"10px"});
+    $("#profile-handle-label").css({"font-size":"12px", "margin-left":"10px"});
+
+    $(".button-profile-interaction").animate({"height": "35px", "width":"35px"});
+    $(".button-profile-interaction").css({"pointer-events":"auto"});
+
+    $("#profile-button-connect").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"/></svg>`);
+    $("#profile-button-edit").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"/></svg>`);
+    $("#profile-button-message").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M4 20q-.825 0-1.412-.587Q2 18.825 2 18V6q0-.825.588-1.412Q3.175 4 4 4h16q.825 0 1.413.588Q22 5.175 22 6v12q0 .825-.587 1.413Q20.825 20 20 20Zm8-7L4 8v10h16V8Zm0-2 8-5H4ZM4 8V6v12Z"/></svg>`);
+    $("#profile-button-about").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M1 20v-4q0-.85.588-1.425Q2.175 14 3 14h3.275q.5 0 .95.25.45.25.725.675.725.975 1.788 1.525Q10.8 17 12 17q1.225 0 2.288-.55 1.062-.55 1.762-1.525.325-.425.762-.675.438-.25.913-.25H21q.85 0 1.425.575Q23 15.15 23 16v4h-7v-2.275q-.875.625-1.887.95Q13.1 19 12 19q-1.075 0-2.1-.337-1.025-.338-1.9-.963V20Zm11-4q-.95 0-1.8-.438-.85-.437-1.425-1.212-.425-.625-1.062-.987Q7.075 13 6.325 13q.55-.925 2.325-1.463Q10.425 11 12 11q1.575 0 3.35.537 1.775.538 2.325 1.463-.725 0-1.375.363-.65.362-1.075.987-.55.8-1.4 1.225Q12.975 16 12 16Zm-8-3q-1.25 0-2.125-.875T1 10q0-1.275.875-2.138Q2.75 7 4 7q1.275 0 2.138.862Q7 8.725 7 10q0 1.25-.862 2.125Q5.275 13 4 13Zm16 0q-1.25 0-2.125-.875T17 10q0-1.275.875-2.138Q18.75 7 20 7q1.275 0 2.138.862Q23 8.725 23 10q0 1.25-.862 2.125Q21.275 13 20 13Zm-8-3q-1.25 0-2.125-.875T9 7q0-1.275.875-2.138Q10.75 4 12 4q1.275 0 2.137.862Q15 5.725 15 7q0 1.25-.863 2.125Q13.275 10 12 10Z"/></svg>`);
+    $("#profile-interaction-container").css({"grid-column": "3", "grid-template-columns": "40px 40px 40px", "margin-top":"0px", "text-align":"center", "margin-bottom": "0px", "height":"80px"})
+
+    $(".swipe-up-element").animate({"top": "0", "opacity": "0"}, "fast");
+    $(".swipe-up-element").fadeOut("fast");
+    
+    $("#profile-page-splash").animate({"height":"120px"});
+    $("#profile-page-splash").css({"height":"120px", "pointer-events":"none"});
+    $(".large-profile-image").animate({"height":"50px", "width":"50px", "margin-left":"5px"});
+
+    $(".large-profile-image").css({"grid-column":"1"});
+    $("#profile-name-splash").css({"grid-column":"2", "height": "50px"});
+    $(".profile-interaction-container").css({"grid-column": "3", "grid-template-columns": "40px 40px 40px", "margin-top":"0px", "text-align":"center", "height":"80px"})
+    
+
+    $("#profile-bio-container").remove();
+
+    initUI();
+    headerDropIn();
+
+
+    console.log(new_feed)
+    yb_showMiniBar(); 
+    yb_showMenuTask();   
+    
+}
+
+
 
 //Profile Page
 function yb_BuildProfile(profile_data){
@@ -184,7 +231,7 @@ function yb_BuildProfile(profile_data){
     let profile_button_about = yb_createElement("button", "profile-button-about", "button-profile-interaction");
     profile_button_about.setAttribute("style", `background-color: ${custom.primary_color}; color: ${custom.title_color};`);
     profile_button_about.setAttribute("data-id", user_id);
-    profile_button_about.innerHTML = "About Me";
+    profile_button_about.innerHTML = "Info";
     profile_interaction_container.appendChild(profile_button_about);
 
     let bio_container = yb_createElement("div", "profile-bio-container", "splash-bio-container");
@@ -238,104 +285,14 @@ function yb_BuildProfile(profile_data){
             if (deltaY < 0) {
                 // Perform actions to exit fullscreen
                 // Add your code here to handle fullscreen exit
-                console.log("clicked")
-                let profile_splash = document.getElementById("profile-page-splash");
-                let image = document.getElementById("profile-image-splash");
-                let profile_id = yb_getSessionValues("profile-username");
-                
-            
-                $("#profile-splash-label").animate({"height": "80px", "top":"100px", "width": "100%"});
-                $("#splash-bio-container").css({"display": "none"});
-                $("#profile-splash-label").css({"display": "grid", "text-align":"left", "grid-template-columns":"60px auto 120px", "padding-top":"230px"});
-                $("#profile-name-header").css({"font-size":"15px", "margin-left":"10px", "margin-top":"10px"});
-                $("#profile-handle-label").css({"font-size":"12px", "margin-left":"10px"});
-            
-                $(".button-profile-interaction").animate({"height": "35px", "width":"35px"});
-                $(".button-profile-interaction").css({"pointer-events":"auto"});
-            
-                $("#profile-button-connect").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"/></svg>`);
-                $("#profile-button-edit").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"/></svg>`);
-                $("#profile-button-message").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M4 20q-.825 0-1.412-.587Q2 18.825 2 18V6q0-.825.588-1.412Q3.175 4 4 4h16q.825 0 1.413.588Q22 5.175 22 6v12q0 .825-.587 1.413Q20.825 20 20 20Zm8-7L4 8v10h16V8Zm0-2 8-5H4ZM4 8V6v12Z"/></svg>`);
-                $("#profile-button-about").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M1 20v-4q0-.85.588-1.425Q2.175 14 3 14h3.275q.5 0 .95.25.45.25.725.675.725.975 1.788 1.525Q10.8 17 12 17q1.225 0 2.288-.55 1.062-.55 1.762-1.525.325-.425.762-.675.438-.25.913-.25H21q.85 0 1.425.575Q23 15.15 23 16v4h-7v-2.275q-.875.625-1.887.95Q13.1 19 12 19q-1.075 0-2.1-.337-1.025-.338-1.9-.963V20Zm11-4q-.95 0-1.8-.438-.85-.437-1.425-1.212-.425-.625-1.062-.987Q7.075 13 6.325 13q.55-.925 2.325-1.463Q10.425 11 12 11q1.575 0 3.35.537 1.775.538 2.325 1.463-.725 0-1.375.363-.65.362-1.075.987-.55.8-1.4 1.225Q12.975 16 12 16Zm-8-3q-1.25 0-2.125-.875T1 10q0-1.275.875-2.138Q2.75 7 4 7q1.275 0 2.138.862Q7 8.725 7 10q0 1.25-.862 2.125Q5.275 13 4 13Zm16 0q-1.25 0-2.125-.875T17 10q0-1.275.875-2.138Q18.75 7 20 7q1.275 0 2.138.862Q23 8.725 23 10q0 1.25-.862 2.125Q21.275 13 20 13Zm-8-3q-1.25 0-2.125-.875T9 7q0-1.275.875-2.138Q10.75 4 12 4q1.275 0 2.137.862Q15 5.725 15 7q0 1.25-.863 2.125Q13.275 10 12 10Z"/></svg>`);
-                $("#profile-interaction-container").css({"grid-column": "3", "grid-template-columns": "40px 40px 40px", "margin-top":"0px", "text-align":"center", "margin-bottom": "0px", "height":"80px"})
-            
-                $(".swipe-up-element").animate({"top": "0", "opacity": "0"}, "fast");
-                $(".swipe-up-element").fadeOut("fast");
-                
-                $("#profile-page-splash").animate({"height":"120px"});
-                $("#profile-page-splash").css({"height":"120px", "pointer-events":"none"});
-                $(".large-profile-image").animate({"height":"50px", "width":"50px", "margin-left":"5px"});
-            
-                $(".large-profile-image").css({"grid-column":"1"});
-                $("#profile-name-splash").css({"grid-column":"2", "height": "50px"});
-                $(".profile-interaction-container").css({"grid-column": "3", "grid-template-columns": "40px 40px 40px", "margin-top":"0px", "text-align":"center", "height":"80px"})
-                
-        
-                $("#profile-bio-container").remove();
-        
-                initUI();
-                headerDropIn();
-
-            
-                console.log(new_feed)
-                yb_showMiniBar(); 
-                yb_showMenuTask();   
-                
+                yb_enterProfile();
             }
         });
         
     });
 
     swipe_up_element.addEventListener("click", function(){
-        console.log("clicked")
-        let profile_splash = document.getElementById("profile-page-splash");
-        let image = document.getElementById("profile-image-splash");
-        let profile_id = yb_getSessionValues("profile-username");
-
-        
-    
-        $("#profile-splash-label").animate({"height": "80px", "top":"100px", "width": "100%"});
-        $("#splash-bio-container").css({"display": "none"});
-        $("#profile-splash-label").css({"display": "grid", "text-align":"left", "grid-template-columns":"60px auto 120px", "padding-top":"230px"});
-        $("#profile-name-header").css({"font-size":"15px", "margin-left":"10px", "margin-top":"10px"});
-        $("#profile-handle-label").css({"font-size":"12px", "margin-left":"10px"});
-    
-        $(".button-profile-interaction").animate({"height": "35px", "width":"35px"});
-        $(".button-profile-interaction").css({"pointer-events":"auto"});
-    
-        $("#profile-button-connect").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"/></svg>`);
-        $("#profile-button-edit").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M5 19h1.4l8.625-8.625-1.4-1.4L5 17.6ZM19.3 8.925l-4.25-4.2 1.4-1.4q.575-.575 1.413-.575.837 0 1.412.575l1.4 1.4q.575.575.6 1.388.025.812-.55 1.387ZM17.85 10.4 7.25 21H3v-4.25l10.6-10.6Zm-3.525-.725-.7-.7 1.4 1.4Z"/></svg>`);
-        $("#profile-button-message").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M4 20q-.825 0-1.412-.587Q2 18.825 2 18V6q0-.825.588-1.412Q3.175 4 4 4h16q.825 0 1.413.588Q22 5.175 22 6v12q0 .825-.587 1.413Q20.825 20 20 20Zm8-7L4 8v10h16V8Zm0-2 8-5H4ZM4 8V6v12Z"/></svg>`);
-        $("#profile-button-about").html(`<svg class="profile-interact-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path style="fill:{{profile.bit_background}};" d="M1 20v-4q0-.85.588-1.425Q2.175 14 3 14h3.275q.5 0 .95.25.45.25.725.675.725.975 1.788 1.525Q10.8 17 12 17q1.225 0 2.288-.55 1.062-.55 1.762-1.525.325-.425.762-.675.438-.25.913-.25H21q.85 0 1.425.575Q23 15.15 23 16v4h-7v-2.275q-.875.625-1.887.95Q13.1 19 12 19q-1.075 0-2.1-.337-1.025-.338-1.9-.963V20Zm11-4q-.95 0-1.8-.438-.85-.437-1.425-1.212-.425-.625-1.062-.987Q7.075 13 6.325 13q.55-.925 2.325-1.463Q10.425 11 12 11q1.575 0 3.35.537 1.775.538 2.325 1.463-.725 0-1.375.363-.65.362-1.075.987-.55.8-1.4 1.225Q12.975 16 12 16Zm-8-3q-1.25 0-2.125-.875T1 10q0-1.275.875-2.138Q2.75 7 4 7q1.275 0 2.138.862Q7 8.725 7 10q0 1.25-.862 2.125Q5.275 13 4 13Zm16 0q-1.25 0-2.125-.875T17 10q0-1.275.875-2.138Q18.75 7 20 7q1.275 0 2.138.862Q23 8.725 23 10q0 1.25-.862 2.125Q21.275 13 20 13Zm-8-3q-1.25 0-2.125-.875T9 7q0-1.275.875-2.138Q10.75 4 12 4q1.275 0 2.137.862Q15 5.725 15 7q0 1.25-.863 2.125Q13.275 10 12 10Z"/></svg>`);
-        $("#profile-interaction-container").css({"grid-column": "3", "grid-template-columns": "40px 40px 40px", "margin-top":"0px", "text-align":"center", "margin-bottom": "0px", "height":"80px"})
-    
-        $(".swipe-up-element").animate({"top": "0", "opacity": "0"}, "fast");
-        $(".swipe-up-element").fadeOut("fast");
-        
-        $("#profile-page-splash").animate({"height":"120px"});
-        $("#profile-page-splash").css({"height":"120px", "pointer-events":"none"});
-        $(".large-profile-image").animate({"height":"50px", "width":"50px", "margin-left":"5px"});
-    
-        $(".large-profile-image").css({"grid-column":"1"});
-        $("#profile-name-splash").css({"grid-column":"2", "height": "50px"});
-        $(".profile-interaction-container").css({"grid-column": "3", "grid-template-columns": "40px 40px 40px", "margin-top":"0px", "text-align":"center", "height":"80px"})
-        
-
-        $("#profile-bio-container").remove();
-
-        initUI();
-        headerDropIn();
-        let new_feed = {
-            "type": "global",
-            "id": profile_id,
-            "filter":"-fo-fr-me-p-c",
-            "sort":"chrono",
-        };
-    
-        console.log(new_feed)
-        yb_showMiniBar(); 
-        yb_showMenuTask();   
-        yb_getFeed(new_feed, "none", false);
+        yb_enterProfile();
     });
 
     return {'splash':profile_splash, 'data':data_element}
