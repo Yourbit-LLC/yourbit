e13baa303df7179cbf37a7fddd08dde402d85bb5
achaney55 <aus.chaney@gmail.com>
2023-09-01 21:35:51 -0400

    Update viewsets.py
    Fixed issue retrieving wrong bits

--- user_profile/api/viewsets.py
+++ user_profile/api/viewsets.py
@@ -146,7 +146,7 @@ class BitViewSet(viewsets.ViewSet):
                         bit_pool = Bit.objects.filter(profile = this_profile).order_by("-time")
                     
                     else:
-                        bit_pool = Bit.objects.filter(profile = user_profile, is_public = True).order_by("-time")
+                        bit_pool = Bit.objects.filter(profile = this_profile, is_public = True).order_by("-time")
 
             else:
                 #Dataset is used for aggregating results based on filters within saved or interacted bits
