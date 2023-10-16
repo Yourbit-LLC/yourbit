89d3156f47827d99680d425af9dc3db5c7647beb
achaney55 <aus.chaney@gmail.com>
2023-10-16 04:59:34 -0400

    changed grid for ui option

--- main/static/scripts/yb-routes.js
+++ main/static/scripts/yb-routes.js
@@ -364,6 +364,22 @@ function customize_url(data){
 
 }
 
+//customize
+function customize_profile_url(data=null){
+    try {
+        yb_purgeScripts(yb_clearContainer);
+    } catch (error) {
+        console.log(error);
+    }
+    console.log("function");
+    
+    $("#content-container").load(`${base_url}/profile/templates/customize-html-profile/`);
+    yb_setSessionValues("location","customize-profile");
+    history.pushState({}, "", `/profile/customize/profile/`);
+    
+
+}
+
 //profile info
 function settings_profile_url(data){
     $("#content-container").html('');
