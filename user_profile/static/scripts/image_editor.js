05d3a573fcc73b130f0cd7adb3e185973bbca2f0
achaney55 <aus.chaney@gmail.com>
2023-10-18 03:41:38 -0400

    added image grid selector to routes

--- user_profile/static/scripts/image_editor.js
+++ user_profile/static/scripts/image_editor.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
     let enable_transparency = document.getElementById("enable-transparency");
     let enable_border = document.getElementById("enable-border");
 });
+
+function previewImage(source){
+    let viewer = yb_createElement('div', 'photo-viewer', 'photo-viewer');
+    viewer.setAttribute("style", "display: none");
+    let viewing_image = yb_renderImage(source, "full-screen-image", "full-screen-image");
+    viewer.appendChild(viewing_image);
+    document.body.appendChild(viewer);
+
+    $('.photo-viewer').fadeIn(200);
+    $('.photo-viewer').animate({"top": "0px"}, 200);
+
+    let select_button = yb_createElement("button", "select-button", "select-button");
+}
