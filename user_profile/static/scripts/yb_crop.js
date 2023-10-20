d39615374b4250491fb6ab800af92fee5da32615
achaney55 <aus.chaney@gmail.com>
2023-10-20 05:11:02 -0400

    made some changes to routes and crop to deal with identifying cropper use case

--- user_profile/static/scripts/yb_crop.js
+++ user_profile/static/scripts/yb_crop.js
@@ -45,7 +45,7 @@ function previewImage(type, method) {
                 crop: function(event) {
                     console.log(event.detail.width);
                     console.log(event.detail.height);
-                    setTimeout(cropImage, 100, type);
+                    cropImage(type);
                 }
             });
         };
