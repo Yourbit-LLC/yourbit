4c5451e240caf0925bbe26d2d4f39e6eb4058083
achaney55 <aus.chaney@gmail.com>
2023-10-24 04:04:29 -0400

    made some changes

--- main/static/scripts/yb-builders.js
+++ main/static/scripts/yb-builders.js
@@ -141,7 +141,7 @@ function BuildBit(bit, liked_bits, disliked_bits){
     let custom = bit.custom;
 
     //Profile Image
-    let profile_image = custom.image_thumbnail_small;
+    let profile_image = custom.custom.image_thumbnail_small;
 
     //colors
     let primary_color = custom.primary_color;
