b2c4f6754b43b7a786476aedd6b1ad88fad6cdf8
achaney55 <aus.chaney@gmail.com>
2024-03-07 16:16:50 -0500

    registered the signals.py file in yb_notify within the apps.py

--- yb_notify/apps.py
+++ yb_notify/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class YbNotifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'yb_notify'
+    
+    def ready(self):
+        import yb_notify.signals
