95eee7f6455031525f1a75457083f161e69ca00c
achaney55 <aus.chaney@gmail.com>
2024-03-12 00:55:07 -0400

    added notification core serializer and notification core viewset

--- yb_notify/routers.py
+++ yb_notify/routers.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .api.viewsets import NotificationViewSet
+from .api.viewsets import NotificationViewSet, NotificationCoreViewSet
 
 router = DefaultRouter()
 router.register(r'notifications', NotificationViewSet)
+router.register(r'notification-core', NotificationCoreViewSet)
 
 # Add more endpoints as needed
 urlpatterns = router.urls
