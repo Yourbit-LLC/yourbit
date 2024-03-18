ef3c0395f6e6550d7ab01c95bdfda9653bbb3faf
achaney55 <aus.chaney@gmail.com>
2024-03-17 23:31:45 -0400

    Fixed missing line in yb_messages/routers.py file.
    Added urlpatterns = router.urls to line 9

--- yb_messages/routers.py
+++ yb_messages/routers.py
@@ -1,1 +1,9 @@
 from rest_framework.routers import DefaultRouter
+from api.viewsets import MessageViewSet, ConversationViewSet
+
+router = DefaultRouter()
+
+router.register(r'messages', MessageViewSet, basename='message')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+urlpatterns = router.urls
