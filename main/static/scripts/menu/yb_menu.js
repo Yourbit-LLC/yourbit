ec43235900eb78a311b8db94710c804f59904a1a
achaney55 <aus.chaney@gmail.com>
2024-03-29 02:17:16 -0400

    ahbasdkjbasf

--- main/static/scripts/menu/yb_menu.js
+++ main/static/scripts/menu/yb_menu.js
@@ -127,7 +127,7 @@ function yb_messageButton() {
 }
 
 const BUTTON_FUNCTIONS = {
-    "home": yb_goHome,
+    "bitstream": yb_goHome,
     "messages": yb_messageButton,
     "people": yb_showPeoplePage,
     "stuff": yb_showStuffPage,
