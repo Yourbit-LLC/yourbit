1f491e6079f688095730a6131642403226fdc781
achaney55 <aus.chaney@gmail.com>
2024-04-03 23:19:35 -0400

    aksad

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main.views import index, create_menu_template, create_object_template, Crea
 
 urlpatterns = [
     path("create-menu/", create_menu_template, name="create"),
-    path("create/<str:object>/", create_object_template, name="index"),
-    # path("create/", CreateElement.as_view(), name="index"),
+    path("templates/create/<str:object>/", create_object_template, name="index"),
+    path("create/", CreateElement.as_view(), name="index"),
 ]
 
