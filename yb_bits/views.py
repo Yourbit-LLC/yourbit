8b439536593ed83e2d69ff838a92ab4285d9db6c
achaney55 <aus.chaney@gmail.com>
2024-04-07 21:00:17 -0400

    updated utility and trying new thing in photo uploads

--- yb_bits/views.py
+++ yb_bits/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from .forms import BitForm
 
+
 # Create your views here.
 def bit_builder_view(request):
     bit_form = BitForm()
@@ -10,3 +11,66 @@ def bit_builder_view(request):
 def bitstream_view(request):
     return render(request, "yb_bits/yb_bitStream.html")
 
+# class PublishMediaBit(View):
+    
+#     def post(request, *args, **kwargs):
+#         from yb_photo.models import Photo
+#         from yb_photo.utility import generate_large_thumbnail, generate_medium_thumbnail, generate_small_thumbnail, generate_tiny_thumbnail
+    
+#         if type == 'photo':
+
+#             #Get file from request
+#             source_file = request.FILES.get('photo')
+
+#             #Create photo instance
+#             new_photo = Photo()
+
+#             #Set new photo file to object
+#             new_photo.image = source_file
+
+#             #512x512
+#             new_photo.large_thumbnail = generate_large_thumbnail(request, source_file)
+            
+#             #256x256
+#             new_photo.medium_thumbnail = generate_medium_thumbnail(request, source_file)
+            
+#             #128x128
+#             new_photo.small_thumbnail = generate_small_thumbnail(request, source_file)
+
+#             #Assign photo object to user
+#             new_photo.user = request.user
+
+#             #Save photo object
+#             new_photo.save()
+            
+
+#             if body != 'yb-no-body':
+#                 new_bit.body = body
+            
+#         else:
+
+#             video = request.FILES.get('video')
+
+#             # Generate a unique file name using uuid or any other logic
+#             unique_filename = str(uuid.uuid4()) + os.path.splitext(video.name)[1]
+
+#             video_object.video.save(unique_filename, video)
+
+#             video_object = Video.objects.create(
+#                 user=request.user, 
+#                 video = video, 
+#                 video_key = unique_filename
+#             )
+
+#             if request.FILES.get('thumbnail_image'):
+#                 thumbnail_image = request.FILES.get('thumbnail_image')
+#                 print(thumbnail_image)
+#                 video_object.thumbnail_image = thumbnail_image
+
+#             # else:
+#             #     video_object.thumbnail_image = generate_video_thumbnail(video)
+
+#             video_object.save()
+
+#             new_bit.video.add(video_object)
+         
--- yb_photo/utility.py
+++ yb_photo/utility.py
@@ -9,7 +9,7 @@ from .models import Photo
 
 # Create your views here.
 def generate_tiny_thumbnail(request, source_file):
-    #Create small thumbnail
+    #Create small thumbnail (32x32)
     source_file = Image.open(source_file)
     tiny_image = source_file.copy()
     sthumb_io = BytesIO()
@@ -25,7 +25,7 @@ def generate_tiny_thumbnail(request, source_file):
     return inmemory_uploaded_file
 
 def generate_small_thumbnail(request, source_file):
-    #Create small thumbnail
+    #Create small thumbnail (64x64)
     source_file = Image.open(source_file)
     small_image = source_file.copy()
     sthumb_io = BytesIO()
@@ -41,7 +41,7 @@ def generate_small_thumbnail(request, source_file):
     return inmemory_uploaded_file
 
 def generate_medium_thumbnail(request, source_file):
-    #Create large thumbnail
+    #Create Medium thumbnail (256x256)
     source_file = Image.open(source_file)
     large_image = source_file.copy()
     lthumb_io = BytesIO()
@@ -56,9 +56,9 @@ def generate_medium_thumbnail(request, source_file):
     
     return inmemory_uploaded_file
 
-#Typicall used for feed thyumbnails
+#Typically used for feed thumbnails
 def generate_large_thumbnail(request, source_file):
-    #Create xlarge thumbnail
+    #Create large thumbnail (512x512)
     source_file = Image.open(source_file)
     xlarge_image = source_file.copy()
     xthumb_io = BytesIO()
@@ -76,13 +76,20 @@ def generate_large_thumbnail(request, source_file):
 
 def process_image(request, source_image = None, cropped_image = None, is_private = False):
 
+    print("Processing image...")
+
     #Create new photo instance
     new_photo = Photo(image=source_image)
+
     new_photo.small_thumbnail = generate_small_thumbnail(request, cropped_image)
     new_photo.medium_thumbnail = generate_medium_thumbnail(request, cropped_image)
     new_photo.large_thumbnail = generate_tiny_thumbnail(request, cropped_image)
 
+    print("Images Cropped")
+
     new_photo.is_private = is_private
     new_photo.save()
+
+    print("Processing Complete: \n\n" + new_photo)
 
     return new_photo
