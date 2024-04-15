36772a1eed568a6e15ac70404b57e99fc60cbdc0
achaney55 <aus.chaney@gmail.com>
2024-04-14 20:55:57 -0400

    hajbsaasoifeoi

--- yb_messages/admin.py
+++ yb_messages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Conversation
+from .models import Conversation, MessageCore
 # Register your models here.
 
 admin.site.register(Conversation)
+admin.site.register(MessageCore)
