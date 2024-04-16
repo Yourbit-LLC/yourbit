e6ce9f21a4ab5478729120fbc3b55b490765327f
achaney55 <aus.chaney@gmail.com>
2024-04-16 02:57:11 -0400

    restyling some messaging stuff

--- yb_messages/admin.py
+++ yb_messages/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Conversation, MessageCore
+from .models import Conversation, MessageCore, Message
 # Register your models here.
 
 admin.site.register(Conversation)
 admin.site.register(MessageCore)
+admin.site.register(Message)
