2fbcc90a7b08b008ad4ef8de55356cdcd11846be
achaney55 <aus.chaney@gmail.com>
2024-04-17 23:05:30 -0400

    sjnfasd

--- yb_messages/api/viewsets.py
+++ yb_messages/api/viewsets.py
@@ -58,7 +58,8 @@ class ConversationViewSet(viewsets.ModelViewSet):
             if member == '':
                 members_split.remove(member)
                 
-
+        members += self.request.user.id
+        
         print(str(start) + ": " + member)
         # Check if the conversation already exists with the same members
 
