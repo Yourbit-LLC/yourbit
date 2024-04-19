0a1920797b8db70ea1dc05e5008a48baa681df0c
achaney55 <aus.chaney@gmail.com>
2024-04-18 20:15:57 -0400

    andjnas

--- yb_bits/static/scripts/yb_bits/yb_bits.js
+++ yb_bits/static/scripts/yb_bits/yb_bits.js
@@ -189,6 +189,14 @@ function createHeader(bit) {
     timeLabel.innerHTML = `${displayDate}<br><small>${displayTime}</small>`;
     header.appendChild(timeLabel);
 
+    const ybMoreButton = yb_createElement("div", "yb-button-bitOptions", `bit-options-${bit.id}`)
+    ybMoreButton.setAttribute("data-catid", bit.id);
+    ybMoreButton.innerHTML = `
+        <svg "yb-icon xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path class="yb-autoFill" d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z"/></svg>
+    `
+    
+    header.appendChild(ybMoreButton)
+
     return header;
 }
 
@@ -492,6 +500,7 @@ function yb_buildBit(bit){
 
     //Title
     new_bit.appendChild(createTitle(bit));
+
 
 
     //Content Attachments
