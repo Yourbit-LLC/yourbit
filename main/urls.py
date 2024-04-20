d191807eee0e3cd3a2e073fe70579fc113097402
achaney55 <aus.chaney@gmail.com>
2024-04-20 16:02:20 -0400

    made some adjustments to styling of the cluster form

--- main/urls.py
+++ main/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("templates/create/bit/", create_bit_template, name="create-object"),
     path("templates/create/cluster/", CreateCluster.as_view(), name="create_cluster"),
     path("templates/create/orbit/", CreateOrbit.as_view(), name="create-orbit"),
+    path("create/cluster/", CreateCluster.as_view(), name="post_cluster"),
+    path("create/cluster/", CreateOrbit.as_view(), name="post_orbit"),
     path("create/", CreateElement.as_view(), name="create-element"),
 ]
 
