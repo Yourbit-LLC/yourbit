0fe39cee97f16770e76da4a88834e3111ad96870
achaney55 <aus.chaney@gmail.com>
2024-04-29 01:20:39 -0400

    Added bug report and feature request pages

--- yb_support/admin.py
+++ yb_support/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Issue
+from .models import Issue, BugReport, UserReport, FeatureRequest
 
 # Register your models here.
 admin.site.register(Issue)
+admin.site.register(BugReport)
+admin.site.register(UserReport)
+admin.site.register(FeatureRequest)
