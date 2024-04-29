be143e3000e15643a183000f18f2c4f3eb0a4e02
achaney55 <aus.chaney@gmail.com>
2024-04-29 00:16:49 -0400

    Added bug report and feature request pages

--- yb_support/urls.py
+++ yb_support/urls.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path, include
 from main.views import index, create_menu_template
-from .views import support_center_template
+from .views import support_center_template, report_bug_template, feature_request_template, contact_info_template
 
 urlpatterns = [
     path("templates/support-center/", support_center_template, name="support_center"),
-    path("templates/report-bug/", create_menu_template, name="report_bug"),
-    path("templates/request-feature/", create_menu_template, name="feature_request"),
-    path("templates/contact-info/", create_menu_template, name="feature_request"),
+    path("templates/report-bug/", report_bug_template, name="report_bug"),
+    path("templates/request-feature/", feature_request_template, name="feature_request"),
+    path("templates/contact-info/", contact_info_template, name="feature_request"),
     path("help/", create_menu_template, name="help"),
 
 ]
