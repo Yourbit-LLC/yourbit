44b21e86f03840a790857658c4716ac9e1496488
achaney55 <aus.chaney@gmail.com>
2024-05-26 12:54:06 -0400

    fixed comment buttons

--- yb_bits/static/scripts/yb_bits/yb_bits.js
+++ yb_bits/static/scripts/yb_bits/yb_bits.js
@@ -516,7 +516,7 @@ function yb_buildComment(comment){
             `delete-comment-${comment_id}`, 
         );
         
-        comment_delete_button.innerHTML = `&times;`;
+        comment_delete_button.innerHTML = `<svg class="yb-center-transform all yb-icon small" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
         comment_delete_button.setAttribute("data-catid", comment_id);
         comment_delete_button.setAttribute("data-bitid", comment.bit);
         comment_bubble.appendChild(comment_delete_button);
