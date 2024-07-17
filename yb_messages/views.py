22afee06ee3c5bd1aa19283d2011bef9bea9065d
achaney55 <aus.chaney@gmail.com>
2024-07-17 03:39:36 -0400

    adsbofb

--- yb_messages/views.py
+++ yb_messages/views.py
@@ -181,7 +181,7 @@ class ConversationView(View):
 
         this_conversation = Conversation.objects.get(id = this_id)
         
-        messages = Message.objects.filter(conversation = this_conversation).order_by("-time")
+        messages = Message.objects.filter(conversation = this_conversation).order_by("time")
 
         members = this_conversation.members.all()
 
