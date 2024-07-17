5d41425e3e888470a9ae1206160a981eff41f401
achaney55 <aus.chaney@gmail.com>
2024-07-17 04:46:42 -0400

    Added checks for custom bits

--- yb_bits/static/scripts/yb_bits/yb_bits4.js
+++ yb_bits/static/scripts/yb_bits/yb_bits4.js
@@ -117,14 +117,14 @@ function createHeader(bit) {
     let profile_image = images.profile_image;
     let thumbnail = profile_image.small_thumbnail;
     
-    if (yb_getCustomValues("bit-colors-on")){
+    if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
         profileImageContainer.style.borderColor = bit.custom.accent_color;
     }
     profileImageContainer.innerHTML = `<img style="object-fit:fill; border-radius: 50%; width: 100%;" src="${thumbnail}">`;
     header.appendChild(profileImageContainer);
 
     const userInfo = yb_createElement("p", "yb-userInfo-bit", `bit-info-${bit.id}`);
-    if (yb_getCustomValues("bit-colors-on")){
+    if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
         userInfo.innerHTML = `<strong class="bit-name" style="color:${bit.custom.title_color};">${bit.display_name}</strong><br> <small class="bit-username" style="color:${bit.custom.text_color};">@${bit.user.username}</small>`;    
     }
     else {
@@ -216,7 +216,7 @@ function yb_createBody(bit) {
     let custom = bit.custom;
     let text_color = custom.text_color;
     const paragraph = yb_createElement("p", "yb-bodyText-bit yb-autoText font-medium", `bit-bodyText-${bit.id}`);
-    if (yb_getCustomValues("bit-colors-on")){
+    if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
         paragraph.style.color = text_color;
     }
     paragraph.innerHTML = bit.body;
@@ -252,7 +252,7 @@ function createTitle(bit) {
     let custom = bit.custom;
     let title_color;
 
-    if (yb_getCustomValues("bit-colors-on")){
+    if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
         title_color = custom.title_color;
         title.innerHTML = `<h2 class="yb-autoText align-center yb-titleText-bit" style="color: ${title_color};">${bit.title}</h2>`;
     } else {
@@ -293,7 +293,7 @@ function yb_createInteractions(bit) {
     if (bit.is_liked){
         like_button.innerHTML = `<svg id="feedback-icon-source" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path id="like-icon-${bit.id}" class="yb-bit-icon active" style="fill: ${bit.custom.icon_color};" d="M15.5 11Q16.15 11 16.575 10.575Q17 10.15 17 9.5Q17 8.85 16.575 8.425Q16.15 8 15.5 8Q14.85 8 14.425 8.425Q14 8.85 14 9.5Q14 10.15 14.425 10.575Q14.85 11 15.5 11ZM8.5 11Q9.15 11 9.575 10.575Q10 10.15 10 9.5Q10 8.85 9.575 8.425Q9.15 8 8.5 8Q7.85 8 7.425 8.425Q7 8.85 7 9.5Q7 10.15 7.425 10.575Q7.85 11 8.5 11ZM12 17.5Q13.775 17.5 15.137 16.525Q16.5 15.55 17.1 14H15.45Q14.925 14.9 14.025 15.45Q13.125 16 12 16Q10.875 16 9.975 15.45Q9.075 14.9 8.55 14H6.9Q7.5 15.55 8.863 16.525Q10.225 17.5 12 17.5ZM12 22Q9.925 22 8.1 21.212Q6.275 20.425 4.925 19.075Q3.575 17.725 2.788 15.9Q2 14.075 2 12Q2 9.925 2.788 8.1Q3.575 6.275 4.925 4.925Q6.275 3.575 8.1 2.787Q9.925 2 12 2Q14.075 2 15.9 2.787Q17.725 3.575 19.075 4.925Q20.425 6.275 21.212 8.1Q22 9.925 22 12Q22 14.075 21.212 15.9Q20.425 17.725 19.075 19.075Q17.725 20.425 15.9 21.212Q14.075 22 12 22ZM12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12ZM12 20Q15.325 20 17.663 17.663Q20 15.325 20 12Q20 8.675 17.663 6.337Q15.325 4 12 4Q8.675 4 6.338 6.337Q4 8.675 4 12Q4 15.325 6.338 17.663Q8.675 20 12 20Z"/></svg>`;
         like_button.classList.add("active");
-        if (yb_getCustomValues("bit-colors-on")){
+        if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
             like_button.style.backgroundColor = bit.custom.button_color;
         }
         
@@ -328,7 +328,7 @@ function yb_createInteractions(bit) {
         `;
         dislike_button.classList.add("active");
 
-        if (yb_getCustomValues("bit-colors-on")){
+        if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
             dislike_button.style.backgroundColor = bit.custom.button_color;
         }
         
@@ -460,7 +460,7 @@ function yb_buildBit(bit){
     //Prepare new bit by creating an element
     new_bit = yb_createElement("div",`yb-element-background yb-bit-shell ${type}bit`, `bit-${id}`);
     
-    if (yb_getCustomValues("bit-colors-on")){
+    if (yb_getCustomValues("bit-colors-on") && !yb_getCustomValues("bit-colors-on")){
         new_bit.style.backgroundColor = primary_color;
     }
     
