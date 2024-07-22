7a12c831ecab3352580b2baf95867b0037be38dd
achaney55 <aus.chaney@gmail.com>
2024-07-22 16:14:29 -0400

    Update yb_filter_bar.js
    added catch to exception trap

--- yb_bits/static/scripts/yb_bitstream/yb_filter_bar.js
+++ yb_bits/static/scripts/yb_bitstream/yb_filter_bar.js
@@ -1,3 +1,9 @@
 try {
-    var feed_filter_btns = document.querySelectorAll(".ff-button")
-    var sort_btn = document.getElementByID("
+    var feed_filter_btns = document.querySelectorAll(".ff-button");
+    var sort_btn = document.getElementByID("ff-button-sort");
+}
+
+catch {
+
+}
+    
