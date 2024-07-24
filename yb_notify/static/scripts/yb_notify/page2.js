43bc08e442e5d4854a2fd2573c2ffc941469d5b8
achaney55 <aus.chaney@gmail.com>
2024-07-24 02:49:30 -0400

    Working on filter panels

--- yb_notify/static/scripts/yb_notify/page2.js
+++ yb_notify/static/scripts/yb_notify/page2.js
@@ -305,6 +305,10 @@ function yb_getNotificationList(notify_class, show_seen = 'True') {
 
 function yb_handleFilterClick() {
     yb_getNotificationList(this.getAttribute('data-notify-class'));
+    for (let i = 0; i < notify_filter_buttons.length; i++){
+        notify_filter_buttons[i].classList.remove('active');
+    }
+    this.classList.add('active');
 }
 
 $(document).ready(function () {
--- yb_profile/static/scripts/yb_profile/yb_people2.js
+++ yb_profile/static/scripts/yb_profile/yb_people2.js
@@ -23,6 +23,10 @@ $(document).ready(function () {
         filter_buttons[i].addEventListener('click', function() {
             let filter = this.getAttribute("name");
             yb_listPeople(filter);
+            for (let i = 0; i < filter_buttons.length; i++){
+                filter_buttons[i].classList.remove("active");
+            }
+            this.classList.add("active");
         });
     }
 });
