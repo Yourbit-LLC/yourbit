f30e86f826b2428615a4861e0732f51ba8d520c9
achaney55 <aus.chaney@gmail.com>
2024-11-04 16:49:21 -0500

    I think I found the issue testing the solution

--- yb_messages/views.py
+++ yb_messages/views.py
@@ -64,7 +64,7 @@ def message_inbox(request):
 
             if conversation.is_name == True:
                 conversation_data[iteration]["name"] = conversation.name
-                conversation_data[conversation]["image"] = profile.custom.profile_image.small_thumbnail
+                conversation_data[iteration]["image"] = profile.custom.profile_image.small_thumbnail
 
             else:
                 if len(conversation.members.all()) > 2:
