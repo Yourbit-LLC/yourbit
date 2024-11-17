9a860d485b4274a4d6f56553ed8939673543b45e
achaney55 <aus.chaney@gmail.com>
2024-11-16 21:50:30 -0500

    fixed the serializer in create conversation

--- yb_messages/api/serializers.py
+++ yb_messages/api/serializers.py
@@ -18,10 +18,11 @@ class ConversationSerializer(serializers.ModelSerializer):
         fields = ['id', 'members', 'is_name', 'name', 'can_join', 'is_community', 'time_modified']
 
     def create(self, validated_data):
+        from yb_profile.models import Profile
         # Extract members as a comma-separated string and convert to a list of user IDs
         member_ids = validated_data.pop('members').split(',')
         member_ids = [int(id.strip()) for id in member_ids if id.strip().isdigit()]  # Ensure only valid integers are converted
-        members = User.objects.filter(id__in=member_ids)
+        members = Profile.objects.filter(id__in=member_ids)
         conversation = Conversation.objects.create(**validated_data)
         conversation.members.set(members)
         return conversation
