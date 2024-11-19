df1c499af17ebd852bc3d3ac1bc3adb6eba5880f
achaney55 <aus.chaney@gmail.com>
2024-11-19 16:58:23 -0500

    better arrangement of customize filter panel

--- yb_bits/static/scripts/yb_bitstream/yb_sort_panel.js
+++ yb_bits/static/scripts/yb_bitstream/yb_sort_panel.js
@@ -1,35 +1,32 @@
 try {
-    var panel_buttons = document.getElementsByClassName("yb-filter-icon");
-}
-catch (e) {
-    console.log(e);
-    panel_buttons = document.getElementsByClassName("yb-filter-icon");
+    var sort_buttons = document.getElementsByClassName("yb-filter-icon");
+} catch {
+    sort_buttons = document.getElementsByClassName("yb-filter-icon");
 }
 
-// Add event listeners to the filter panel buttons
 $(document).ready(function() {
-    console.log("filter-panel-ready");
+    console.log("sort-panel-ready");
     for (var i = 0; i < panel_buttons.length; i++) {
         panel_buttons[i].addEventListener('click', function() {
             // Toggle the filter panel
-            let filter_option = this.getAttribute("data-filter");
-            let current_filter = yb_getSessionValues("filter");
+            let filter_option = this.getAttribute("data-sort");
+            let current_filter = yb_getSessionValues("sort");
 
             //check if filter option is in the is in the current filter string
-            if (current_filter.includes("-"+filter_option)) {
+            if (current_filter.includes(filter_option)) {
                 //remove the filter option
                 current_filter = current_filter.replace(filter_option, "");
                 //remove the active class
                 this.classList.remove("active");
 
-                yb_setSessionValues("filter", current_filter);
+                yb_setSessionValues("sort", current_filter);
             } else {
                 //add the filter option
                 current_filter += "-"+filter_option;
                 //add the active class
                 this.classList.add("active");
 
-                yb_setSessionValues("filter", current_filter);
+                yb_setSessionValues("sort", current_filter);
             } 
             
             yb_setSessionValues('bitstream-page', 1);
@@ -43,3 +40,4 @@ $(document).ready(function() {
         });
     }
 });
+
