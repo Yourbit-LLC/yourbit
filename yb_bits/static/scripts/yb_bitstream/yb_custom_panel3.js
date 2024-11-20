b3e1140c920c7df9d889a06ab45470cf745775b0
achaney55 <aus.chaney@gmail.com>
2024-11-20 03:20:51 -0500

    added event listener to filter button for bit overrides

--- yb_bits/static/scripts/yb_bitstream/yb_custom_panel3.js
+++ yb_bits/static/scripts/yb_bitstream/yb_custom_panel3.js
@@ -20,6 +20,28 @@ $(document).ready(function() {
                     yb_changeUIState('modded');
                 }
             }
+            if (action == "custom-bit") {
+                //If yb_getsessionvalues("only-my-colors") is "True" set it to false
+                let only_my_colors = yb_getSessionValues("only-my-colors");
+                
+                if (only_my_colors == "True") {
+                    only_my_colors = false
+                } else {
+                    only_my_colors = true
+                }
+
+                if (this.classList.contains("active")) {
+                    this.classList.remove("active");
+                    //Set user default to false, to ignore user color overridem and bit colors to false to remove all color overrides
+                    yb_overrideBitColors(false, false);
+                } else {
+                    this.classList.add("active");
+                    //Set user default to value of only_my_colors to decide if user color overrides should be applied set bit colors to true to apply color overrides
+                    yb_overrideBitColors(only_my_colors, true);
+                }
+
+                
+            }
             
         });
     }
