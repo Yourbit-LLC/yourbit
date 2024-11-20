923e3771bff5b9a39743dea789a0c73d689da1c2
achaney55 <aus.chaney@gmail.com>
2024-11-20 03:33:33 -0500

    added functionality for enabling and disabling the wallpaper from the filter panel

--- yb_bits/static/scripts/yb_bitstream/yb_custom_panel3.js
+++ yb_bits/static/scripts/yb_bitstream/yb_custom_panel3.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
                     yb_changeUIState('modded');
                 }
             }
-            if (action == "custom-bit") {
+            else if (action == "custom-bit") {
                 //If yb_getsessionvalues("only-my-colors") is "True" set it to false
                 let only_my_colors = yb_getSessionValues("only-my-colors");
                 
@@ -34,13 +34,46 @@ $(document).ready(function() {
                     this.classList.remove("active");
                     //Set user default to false, to ignore user color overridem and bit colors to false to remove all color overrides
                     yb_overrideBitColors(false, false);
+                    yb_setSessionValues("bit-colors-on", "False");
                 } else {
                     this.classList.add("active");
                     //Set user default to value of only_my_colors to decide if user color overrides should be applied set bit colors to true to apply color overrides
                     yb_overrideBitColors(only_my_colors, true);
+                    yb_setSessionValues("bit-colors-on", "True");
                 }
 
                 
+            } else if (action == "only-my-colors") {
+                //If yb_getsessionvalues("only-my-colors") is "True" set it to false
+
+                if (this.classList.contains("active")) {
+                    this.classList.remove("active");
+                    yb_overrideBitColors(false, true);
+                    yb_setSessionValues("only-my-colors", "False");
+                } else {
+                    this.classList.add("active");
+                    yb_overrideBitColors(true, true);
+                    yb_setSessionValues("only-my-colors", "True");
+                    
+                }
+            } else if (action == "wallpaper") {
+                if (this.classList.contains("active")) {
+                    this.classList.remove("active");
+                    CONTENT_CONTAINER_A.classList.remove("yb-bg-transparent");
+                    CONTENT_CONTAINER_A.classList.add("yb-bg-autoGray");
+                    CONTENT_CONTAINER_B.classList.remove("yb-bg-transparent");
+                    CONTENT_CONTAINER_B.classList.add("yb-bg-autoGray");
+                    yb_setCustomValues("wallpaper", "False");
+                } else {
+                    this.classList.add("active");
+                    CONTENT_CONTAINER_A.classList.remove("yb-bg-autoGray");
+                    CONTENT_CONTAINER_A.classList.add("yb-bg-transparent");
+                    CONTENT_CONTAINER_B.classList.remove("yb-bg-autoGray");
+                    CONTENT_CONTAINER_B.classList.add("yb-bg-transparent");
+                    let wallpaper = yb_getWallpaper(profile);
+                    yb_loadWallpaper(wallpaper);
+                    yb_setCustomValues("wallpaper", "True");
+                }
             }
             
         });
