e9eaf8bbc6acb23b08e054a3ee4283fdb10ed658
achaney55 <aus.chaney@gmail.com>
2024-11-21 18:55:59 -0500

    worked on continuing to color bits along feed when flat mode is toggled on

--- yb_bits/static/scripts/yb_bits/yb_bits23.js
+++ yb_bits/static/scripts/yb_bits/yb_bits23.js
@@ -591,10 +591,15 @@ function yb_buildBit(bit, interactions=true){
     
     console.log(yb_getSessionValues("bit-colors-on"));   
     console.log(yb_getSessionValues("only-my-colors"));
-    if (yb_getSessionValues("bit-colors-on") == "True" && yb_getSessionValues("only-my-colors") == "False"){
-        new_bit.style.backgroundColor = primary_color;
-    } else if (yb_getCustomValues("flat-mode-on") == "True"){
+
+    
+    if (yb_getCustomValues("flat-mode-on") == "True"){
         new_bit.style.backgroundColor = "transparent";
+    } else {
+        if (yb_getSessionValues("bit-colors-on") == "True" && yb_getSessionValues("only-my-colors") == "False"){
+            new_bit.style.backgroundColor = primary_color;
+        } 
+
     }
     
 
