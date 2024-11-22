6255347c53c21ed6da56c1202ca1b2fd1cfe8378
achaney55 <aus.chaney@gmail.com>
2024-11-22 04:27:11 -0500

    fix to flat mode to remove filters

--- yb_customize/static/scripts/yb_customize/yb_customCore14.js
+++ yb_customize/static/scripts/yb_customize/yb_customCore14.js
@@ -95,7 +95,7 @@ const DEFAULT_STYLESHEET_INDEX = {
     "yb-stylesheet-modifier": "/static/css/main/yb_modifiers4.css",
     "yb-stylesheet-button": "/static/css/main/yb_buttons6.css",
     "yb-stylesheet-container": "/static/css/main/yb_containers9.css",
-    "yb-stylesheet-bit": "/static/css/yb_bits/yb_bits6.css",
+    "yb-stylesheet-bit": "/static/css/yb_bits/yb_bits7.css",
 }
 
 const MODDED_STYLESHEET_INDEX = {
