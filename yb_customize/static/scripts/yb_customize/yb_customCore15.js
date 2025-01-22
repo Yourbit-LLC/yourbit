19447ec77d06042807b774a5e05f4d9397c02261
achaney55 <aus.chaney@gmail.com>
2025-01-22 01:04:01 -0500

    fixed home screen authentication links on mobile app

--- yb_customize/static/scripts/yb_customize/yb_customCore15.js
+++ yb_customize/static/scripts/yb_customize/yb_customCore15.js
@@ -93,7 +93,7 @@
 
 */
 const DEFAULT_STYLESHEET_INDEX = {
-    "yb-stylesheet-core": "/static/css/main/yb_core6.css",
+    "yb-stylesheet-core": "/static/css/main/yb_core7.css",
     "yb-stylesheet-modifier": "/static/css/main/yb_modifiers4.css",
     "yb-stylesheet-button": "/static/css/main/yb_buttons6.css",
     "yb-stylesheet-container": "/static/css/main/yb_containers9.css",
