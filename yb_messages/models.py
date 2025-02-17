a30f72c2fcf3489d51914cd29cc9a0352d2abcf0
achaney55 <aus.chaney@gmail.com>
2025-02-17 17:14:07 -0500

    working on message encryption

--- yb_messages/models.py
+++ yb_messages/models.py
@@ -59,7 +59,7 @@ class Message(models.Model):
     conversation = models.ForeignKey('Conversation', related_name='conversation', on_delete=models.CASCADE, default=None)
     from_user = models.ForeignKey('yb_profile.Profile', related_name="sender", on_delete=models.CASCADE, blank=True)
     body = models.CharField(max_length = 1500, blank=True)
-    encrypted_body = EncryptedTextField(blank=True)
+    encrypted_body = EncryptedTextField(blank=True, null=True)
     videos = models.ManyToManyField('yb_video.Video', blank=True)
     images = models.ManyToManyField('yb_photo.Photo', blank=True)
     time = models.DateTimeField(default=timezone.now)
