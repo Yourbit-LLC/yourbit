91d819436d1085b9ee1356b5584611d02bc208b5
achaney55 <aus.chaney@gmail.com>
2025-02-22 17:58:20 -0500

    Working on a fix for showing message conversations

--- yb_messages/models.py
+++ yb_messages/models.py
@@ -4,6 +4,7 @@ from yb_accounts.models import Account as User
 from django.contrib.postgres.fields import ArrayField as ArrayField
 from main.models import EncryptedTextField
 from main.models import get_cipher
+from cryptography.fernet import InvalidToken
 # Create your models here.
 
 class MessageCore(models.Model):
@@ -71,8 +72,11 @@ class Message(models.Model):
         """Automatically decrypts the body when accessed"""
 
         cipher = get_cipher()
-        decrypted_text = cipher.decrypt(self.body.encode()).decode()
-        return decrypted_text  # This forces decryption
+        try:
+            decrypted_text = cipher.decrypt(self.body.encode()).decode()
+            return decrypted_text  # This forces decryption
+        except InvalidToken:
+            return ""  # Return empty string if decryption fails
 
 class ChatSticker(models.Model):
     user = models.ForeignKey(User, related_name='chat_stickers', on_delete=models.CASCADE, default=None)
