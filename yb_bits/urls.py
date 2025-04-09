479905025c1e39a48de050aa0178f62f47961b3b
achaney55 <aus.chaney@gmail.com>
2025-04-09 00:59:23 -0400

    Modified some URL's and added some documentation for reference on handling them

--- yb_bits/urls.py
+++ yb_bits/urls.py
@@ -3,22 +3,26 @@ from .views import *
 from .api.viewsets import BitFeedAPIView
 
 
-
+#URL patterns for bit formatted content on yourbit. For API routes, see yb_bits/api/routers.py
 urlpatterns = [
-    #URL for API
-    path('api/', include('yb_bits.routers')),
-    path('api/bitstream/', BitFeedAPIView.as_view(), name='bitstream'),
+    
+    #URL for Views (shown in browser)
     path("delete-cluster/", delete_cluster, name="delete-cluster"),
     path("add-to-cluster/", add_to_cluster, name="add-to-cluster"),
     path("remove-from-cluster/", remove_from_cluster, name="remove-from-cluster"),
     path("edit-bit/<int:pk>/", edit_bit_view, name="edit-bit"),
     path("set-user-bitstreams/", set_user_bitstreams, name="set-user-bitstreams"),
-    path("view/<int:id>/", view_bit_landing, name="bit-view"),
-    path("generate-test-bits/<int:amount>/", generate_test_bits, name="generate-testbits"),
+    path("view/<int:id>/", view_bit, name="view-bit"),
     path("templates/image/<int:id>/", view_full_image, name="bit-image"),
-    
 
-    #URL for views
+    #Developer Commands (Legacy, developer commands are now entered using the command line)
+    path("generate-test-bits/<int:amount>/", generate_test_bits, name="generate-testbits"),
+
+    #API URLs (not shown in browser)
+    path('api/', include('yb_bits.routers')), #API Routes (yourbit.me/bits/api/), see yb_bits/api/routers.py 
+    path('api/bitstream/', BitFeedAPIView.as_view(), name='bitstream'),
+
+    #URL for Templates (Not shown in browser)
     path("templates/edit-bit/<int:id>/", edit_bit_view, name="edit-bit"),
     path("templates/list-clusters/", list_clusters, name="list-clusters"),
     path("templates/select-clusters/<int:bit_id>/", select_clusters, name="list-clusters"),
@@ -26,7 +30,7 @@ urlpatterns = [
     path('templates/builder/share/<int:pk>/', bit_builder_share_view, name="bit-builder"),
     path('templates/builder/', bit_builder_view, name="bit-builder"),
     path('templates/bitstream/', bitstream_view, name="bit-feed"),
-    path('templates/bit/focus/<int:pk>/', bit_focus_view, name="bit-focus"),
+    path('templates/bit/view/<int:pk>/', view_bit_template, name="view-bit-template"),
     path('templates/filter/sort/', sort_panel_view, name="filter-panel-sort"),
     path('templates/filter/filter/', filter_panel_view, name="filter-panel-filter"),
     path('templates/filter/customize/', customize_panel_view, name="filter-panel-customize"),
@@ -38,5 +42,5 @@ urlpatterns = [
     path("templates/builder/customize/", BitBuildCustomize.as_view(), name="build-bit-customize"),
     path("templates/builder/thumbnail/", BitThumbnailSetup.as_view(), name="build-bit-customize"),
     path("templates/share-menu/<int:id>/", ShareMenuTemplate.as_view(), name="share-menu-template"),
-    path("templates/view/<int:id>/", view_bit, name="bit-view"),
+
 ]
