c077eab2e5a4b68c701c7d8afb9e8016b56fc8f3
achaney55 <aus.chaney@gmail.com>
2025-04-17 00:08:49 -0400

    made some more celery changes

--- YourbitGold/yb_celery.py
+++ YourbitGold/yb_celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YourbitGold.settings')
 
-app = Celery('yourbit')
+app = Celery('YourbitGold')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
