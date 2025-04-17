f5fad08db71874b65b1e0d3aa8a04748e71705ed
achaney55 <aus.chaney@gmail.com>
2025-04-17 01:18:43 -0400

    rebuilding create forms for cleaner style

--- YourbitGold/settings.py
+++ YourbitGold/settings.py
@@ -218,8 +218,8 @@ FILE_UPLOAD_HANDLERS = [
 ]
 
 # Celery Configuration Options
-CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
