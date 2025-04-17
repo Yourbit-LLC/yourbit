181e2fa21b82dccc3f3bc9d008494cd216d2dd85
achaney55 <aus.chaney@gmail.com>
2025-04-17 16:42:56 -0400

    modifications to profile info

--- yb_settings/views.py
+++ yb_settings/views.py
@@ -176,7 +176,7 @@ class SettingsProfile(View):
         profile_info.year_graduated_hs = request.POST.get('year_graduated_hs') or profile_info.year_graduated_hs
         profile_info.college = request.POST['college']
         profile_info.year_graduated_u = request.POST['year_graduated_u'] or profile_info.year_graduated_u
-        # profile_info.field_of_study = request.POST['field_of_study']
+        profile_info.field_of_study = request.POST['field_of_study']
 
         profile_info.hometown = request.POST['hometown']
         profile_info.country_of_origin = request.POST['country_of_origin']
